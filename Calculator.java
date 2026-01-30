ca84ad200a253935d8ae539cfed72349c1468f50
Sridhar Pedhamanishi <peddamanishisridhar@gmail.com>
2026-01-30 11:12:27 +0530

    Basic calculator operations in Calculator.java
    Fix class name typo and correct output formatting.

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+class Calculaotr
+{
+  public static void main(String args[])
+  {
+    int n1 = Integer.parseInt(args[0]);
+    int n2 = Integer.parseInt(args[1]);
+    System.out.println("Addition is:"+n1+n2);
+    System.out.println("Subtraction is:"+n1-n2);
+    System.out.println("Multiplicaton is:"+n1*n2);
+    System.out.println("Division is:"+n1/n2);
+  }
+}
