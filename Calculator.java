d90da73554da19a10c85445a6ab668a502e3dfd2
Sridhar Pedhamanishi <peddamanishisridhar@gmail.com>
2026-02-18 15:36:31 +0530

    Fix print statement and ensure variable assignment

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,14 @@
-
-+
 class Calculator
 {
   public static void main(String args[])
   {
     int n1 = Integer.parseInt(args[0]);
-    int n2 = Integer.parseInt(args[1]);+
+    int n2 = Integer.parseInt(args[1]);
     System.out.println("Addition is:"+(n1+n2));
     System.out.println("Subtraction is:"+(n1-n2));
     System.out.println("Multiplicaton is:"+(n1*n2));
     System.out.println("Division is:"+(n1/n2));
-    System.out.pr("Changes updated");
+    System.out.println("Changes updated");
   }
 }
 "
