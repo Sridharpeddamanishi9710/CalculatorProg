6648de588a3637899ab154a5e39aeaf41b95e49e
Sridhar Pedhamanishi <peddamanishisridhar@gmail.com>
2026-02-18 14:47:27 +0530

    Implement basic calculator functions in Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+import sys
+n1=int(sys.arv[1])
+n2=int(sys.argv[2])
+r1=n1+n1
+r1=n1+n1
+r2=n1-n1
+r3=n1*n1
+r4=n1/n1
+print("Addition:",r1)
+print("Subtraction:",r2)
+print("Multiplication:",r3)
+print("Division:",r4)
